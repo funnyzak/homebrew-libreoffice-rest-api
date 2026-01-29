323f5d1b6310b8b94d24c5aa6ae5755ef2df7f85
funnyzak <funnyzak@users.noreply.github.com>
2026-01-29 01:43:38 +0000

    chore: update libreoffice-rest-api to v0.0.6

--- Formula/libreoffice-rest-api.rb
+++ Formula/libreoffice-rest-api.rb
@@ -4,9 +4,9 @@
 class LibreofficeRestApi < Formula
   desc "A lightweight document conversion service based on Go that encapsulates Libre Office command line capabilities through REST API."
   homepage "https://github.com/funnyzak/libreoffice-rest-api"
-  url "https://github.com/funnyzak/libreoffice-rest-api/releases/download/0.0.5/libreoffice-rest-api-darwin-amd64.tar.gz"
-  version "0.0.5"
-  sha256 "8136cc41d636782e5f3a109c20a1bb6c8ff5ef9ae90ca928751c55e68495fc2f"
+  url "https://github.com/funnyzak/libreoffice-rest-api/releases/download/0.0.6/libreoffice-rest-api-darwin-amd64.tar.gz"
+  version "0.0.6"
+  sha256 "sha256:dd3c3e65dbcbe3c75970acbe99aea416b7750513cad4715e2a52b53a411720c5"
   license "MIT"
 
   livecheck do
@@ -17,13 +17,13 @@ class LibreofficeRestApi < Formula
   depends_on :macos
 
   on_intel do
-    url "https://github.com/funnyzak/libreoffice-rest-api/releases/download/0.0.5/libreoffice-rest-api-darwin-amd64.tar.gz"
-    sha256 "8136cc41d636782e5f3a109c20a1bb6c8ff5ef9ae90ca928751c55e68495fc2f"
+    url "https://github.com/funnyzak/libreoffice-rest-api/releases/download/0.0.6/libreoffice-rest-api-darwin-amd64.tar.gz"
+    sha256 "sha256:dd3c3e65dbcbe3c75970acbe99aea416b7750513cad4715e2a52b53a411720c5"
   end
 
   on_arm do
-    url "https://github.com/funnyzak/libreoffice-rest-api/releases/download/0.0.5/libreoffice-rest-api-darwin-arm64.tar.gz"
-    sha256 "c8454a4c6a65b6c034475f41990d0be3f92e08953baa10b9617ebeefd445af7c"
+    url "https://github.com/funnyzak/libreoffice-rest-api/releases/download/0.0.6/libreoffice-rest-api-darwin-arm64.tar.gz"
+    sha256 "sha256:dd3c3e65dbcbe3c75970acbe99aea416b7750513cad4715e2a52b53a411720c5"
   end
 
   def install
