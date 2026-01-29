df3f7feed026b3de6e44f22caa4b6a6d977b1502
Leon <silenceace@gmail.com>
2026-01-29 09:28:30 +0800

    chore:update

--- Formula/libreoffice-rest-api.rb
+++ Formula/libreoffice-rest-api.rb
@@ -1,12 +1,12 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class libreoffice-rest-api < Formula
-  desc "A powerful HTTP request debugging tool built in Go"
+class LibreofficeRestApi < Formula
+  desc "A lightweight document conversion service based on Go that encapsulates Libre Office command line capabilities through REST API."
   homepage "https://github.com/funnyzak/libreoffice-rest-api"
-  url "https://github.com/funnyzak/libreoffice-rest-api/releases/download/0.2.2/libreoffice-rest-api-darwin-amd64.tar.gz"
-  version "0.2.2"
-  sha256 "sha256:468748a87f211a7fd7dbf4af4636c237f2c35d16ef3331101a75c5547b35f9e5"
+  url "https://github.com/funnyzak/libreoffice-rest-api/releases/download/0.0.5/libreoffice-rest-api-darwin-amd64.tar.gz"
+  version "0.0.5"
+  sha256 "468748a87f211a7fd7dbf4af4636c237f2c35d16ef3331101a75c5547b35f9e5"
   license "MIT"
 
   livecheck do
@@ -17,13 +17,13 @@ class libreoffice-rest-api < Formula
   depends_on :macos
 
   on_intel do
-    url "https://github.com/funnyzak/libreoffice-rest-api/releases/download/0.2.2/libreoffice-rest-api-darwin-amd64.tar.gz"
-    sha256 "sha256:468748a87f211a7fd7dbf4af4636c237f2c35d16ef3331101a75c5547b35f9e5"
+    url "https://github.com/funnyzak/libreoffice-rest-api/releases/download/0.0.5/libreoffice-rest-api-darwin-amd64.tar.gz"
+    sha256 "468748a87f211a7fd7dbf4af4636c237f2c35d16ef3331101a75c5547b35f9e5"
   end
 
   on_arm do
-    url "https://github.com/funnyzak/libreoffice-rest-api/releases/download/0.2.2/libreoffice-rest-api-darwin-arm64.tar.gz"
-    sha256 "sha256:468748a87f211a7fd7dbf4af4636c237f2c35d16ef3331101a75c5547b35f9e5"
+    url "https://github.com/funnyzak/libreoffice-rest-api/releases/download/0.0.5/libreoffice-rest-api-darwin-arm64.tar.gz"
+    sha256 "468748a87f211a7fd7dbf4af4636c237f2c35d16ef3331101a75c5547b35f9e5"
   end
 
   def install
