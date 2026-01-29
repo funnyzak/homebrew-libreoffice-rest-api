a27038adc548b2d93086b05c687aa9c0370e74c0
Leon <silenceace@gmail.com>
2026-01-29 09:34:34 +0800

    chore:update

--- Formula/libreoffice-rest-api.rb
+++ Formula/libreoffice-rest-api.rb
@@ -6,7 +6,7 @@ class LibreofficeRestApi < Formula
   homepage "https://github.com/funnyzak/libreoffice-rest-api"
   url "https://github.com/funnyzak/libreoffice-rest-api/releases/download/0.0.5/libreoffice-rest-api-darwin-amd64.tar.gz"
   version "0.0.5"
-  sha256 "468748a87f211a7fd7dbf4af4636c237f2c35d16ef3331101a75c5547b35f9e5"
+  sha256 "8136cc41d636782e5f3a109c20a1bb6c8ff5ef9ae90ca928751c55e68495fc2f"
   license "MIT"
 
   livecheck do
@@ -18,12 +18,12 @@ class LibreofficeRestApi < Formula
 
   on_intel do
     url "https://github.com/funnyzak/libreoffice-rest-api/releases/download/0.0.5/libreoffice-rest-api-darwin-amd64.tar.gz"
-    sha256 "468748a87f211a7fd7dbf4af4636c237f2c35d16ef3331101a75c5547b35f9e5"
+    sha256 "8136cc41d636782e5f3a109c20a1bb6c8ff5ef9ae90ca928751c55e68495fc2f"
   end
 
   on_arm do
     url "https://github.com/funnyzak/libreoffice-rest-api/releases/download/0.0.5/libreoffice-rest-api-darwin-arm64.tar.gz"
-    sha256 "468748a87f211a7fd7dbf4af4636c237f2c35d16ef3331101a75c5547b35f9e5"
+    sha256 "c8454a4c6a65b6c034475f41990d0be3f92e08953baa10b9617ebeefd445af7c"
   end
 
   def install
@@ -31,6 +31,6 @@ class LibreofficeRestApi < Formula
   end
 
   test do
-    system "#{bin}/libreoffice-rest-api --version"
+    system "#{bin}/libreoffice-rest-api version"
   end
 end
